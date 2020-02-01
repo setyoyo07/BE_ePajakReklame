b19ba98983adc00ac1ee6a0a21de0e4b22fde03d
setyoyo07 <setyo@alterra.id>
2020-02-01 15:55:35 +0700

    add model laporan, bukti_pembayaran, kode_qr, variabel_perhitungan

---
+++ blueprints/kode_qr/model.py
@@ -0,0 +1,32 @@
+from blueprints import db
+from flask_restful import fields
+from datetime import datetime
+
+
+class KodeQR(db.Model):
+    __tablename__ = "kodeQR"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    bukti_pembayaran_id_id = db.Column(db.Integer, db.ForeignKey("bukti_pembayaran.id"))
+    kode_unik = db.Column(db.String(255), default='')
+    link_gambar = db.Column(db.String(1000), default='')
+    status_scan = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now())
+    
+    response_fields = {
+        "created_at": fields.DateTime,
+        "updated_at": fields.DateTime,
+        "id": fields.Integer,
+        "bukti_pembayaran_id": fields.Integer,
+        "kode_unik": fields.String,
+        "link_gambar": fields.String,
+        "status_scan": fields.Boolean,
+    }
+
+    def __init__(self, bukti_pembayaran_id_id, kode_unik, link_gambar):
+        self.bukti_pembayaran_id_id = bukti_pembayaran_id_id  
+        self.kode_unik = kode_unik  
+        self.link_gambar = link_gambar
+        
+    def __repr__(self):
+        return "<KodeQR %r>" % self.id
---
+++ blueprints/laporan/model.py
@@ -0,0 +1,43 @@
+from blueprints import db
+from flask_restful import fields
+from datetime import datetime
+
+
+class Laporan(db.Model):
+    __tablename__ = "laporan"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    objek_pajak_id = db.Column(db.Integer, db.ForeignKey("objek_pajak.id"))
+    tarif_klasifikasi_jalan = db.Column(db.Integer, default=0)
+    tarif_letak_pemasangan = db.Column(db.Integer, default=0)
+    tarif_njopr = db.Column(db.Integer, default=0)
+    tarif_tingi_tanah = db.Column(db.Integer, default=0)
+    total_pajak = db.Column(db.Integer, default=0)
+    nomor_skpd = db.Column(db.String(255), default='')
+    status_pembayaran = db.Column(db.Boolean, default=False)
+    pembatalan_laporan = db.Column(db.Boolean, default=False)
+    status_verifikasi = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now())
+    
+    response_fields = {
+        "created_at": fields.DateTime,
+        "updated_at": fields.DateTime,
+        "id": fields.Integer,
+        "objek_pajak_id": fields.Integer,
+        "tarif_klasifikasi_jalan": fields.Integer,
+        "tarif_letak_pemasangan": fields.Integer,
+        "tarif_njopr": fields.Integer,
+        "tarif_tinggi_tanah": fields.Integer,
+        "total_pajak": fields.Integer,
+        "nomor_skpd": fields.String,
+        "status_pembayaran": fields.Boolean
+        "pembatalan_laporan": fields.Boolean
+        "status_verifikasi": fields.Boolean
+    }
+
+    def __init__(self, objek_pajak_id, nomor_skpd):
+        self.objek_pajak_id = objek_pajak_id 
+        self.nomor_skpd = nomor_skpd
+        
+    def __repr__(self):
+        return "<Laporan %r>" % self.id
